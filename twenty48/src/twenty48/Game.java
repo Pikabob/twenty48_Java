19d51dd35b7c270e7bfde226fe7210952262df7b
Pikabob <pikabob25@gmail.com>
2014-05-05 11:36:24 -0700

    Cleanup, shortening of shift method

--- twenty48/src/twenty48/Game.java
+++ twenty48/src/twenty48/Game.java
@@ -87,12 +87,7 @@ public class Game {
 					}
 					currentval = p[x][y].getValue();
 					if(lastval == currentval &&  lastval != 0){
-//						p[lastloc[0]][lastloc[1]].setValue(lastval+currentval);
-//						p[x][y].setValue(0);
 						return false;
-//						lastval = 0;
-//						lastloc[0] = -1;
-//						lastloc[1] = -1;
 					}else if(currentval!=0){
 						lastval = currentval;
 						lastloc[0] = x;
@@ -114,29 +109,22 @@ public class Game {
 			}
 			System.out.println("Inputs are wasd. q to quit");
 			String s = in.nextLine();
-//			char dir;
+			char dir;
 			boolean valid = false;
 			if(s.equals(up)){
-//				dir = 'u';
-				valid = move('u');
+				dir = 'u';
 			}else if(s.equals(down)){
-//				dir = 'd';
-				valid = move('d');
+				dir = 'd';
 			}else if(s.equals(left)){
-//				dir = 'l';
-				valid = move('l');
+				dir = 'l';
 			}else if(s.equals(right)){
-//				dir = 'r';
-				valid = move('r');
+				dir = 'r';
 			}else if(s.equals("quit")||s.equals("q")){
 				break;
 			}else{
 				continue;
 			}
-//			if(isValidMove()){
-//				move(dir);
-//			}
-			//this comment is useless<-
+			valid = move(dir);
 			if(valid){
 				putRandom(1);
 			}
@@ -207,252 +195,91 @@ public class Game {
 					lastval = currentval;
 					lastloc[0] = x;
 					lastloc[1] = y;
-//					valid = true;
 				}
 			}
 		}
 		return valid;
-/*
- * up:
- * 0, 0
- * 0, 1
- * 1, 0
- * 1, 1
- * 
- * 
- * down:
- * 0, 1
- * 0, 0
- * 1, 1
- * 1, 0
- * 
- * left;
- * 0, 0
- * 1, 0
- * 0, 1
- * 1, 1
- * 
- * right:
- * 1, 0
- * 0, 0
- * 1, 1
- * 0, 1
- */
-//		for(int i1 = 0; i1<width; i1++){
-//			int i = Math.abs(iMod - i1);
-//			int lastval = 0;
-//			int[] lastloc = {-1, -1};
-//			for(int j1 = 0; j1<height; j1++){
-//				int j = Math.abs(jMod - j1);
-//				int currentval = p[i][j].getValue();
-//				if(lastval == currentval &&  lastval != 0){
-//					p[lastloc[0]][lastloc[1]].setValue(lastval+currentval);
-//					p[i][j].setValue(0);
-//					lastval = 0;
-//					lastloc[0] = -1;
-//					lastloc[1] = -1;
-//				}else if(currentval!=0){
-//					lastval = currentval;
-//					lastloc[0] = i;
-//					lastloc[1] = j;
-//				}
-//			}
-//		}
-			
-//		if(dir == 'u'){
-//			for(int j = width-1; j>=0; j--){
-//				int lastval = 0;
-//				int[] lastloc = {-1, -1};
-//				for(int i = 0; i<height; i++){
-//					int currentval = p[i][j].getValue();
-//					if(lastval == currentval &&  lastval != 0){
-//						p[lastloc[0]][lastloc[1]].setValue(lastval+currentval);
-//						p[i][j].setValue(0);
-//						lastval = 0;
-//						lastloc[0] = -1;
-//						lastloc[1] = -1;
-//					}else if(currentval!=0){
-//						lastval = currentval;
-//						lastloc[0] = i;
-//						lastloc[1] = j;
-//					}
-//				}
-//			}
-//		}else if(dir == 'd'){
-//			for(int j = 0; j<width; j++){
-//				int lastval = 0;
-//				int[] lastloc = {-1, -1};
-//				for(int i = 0; i<height; i++){
-//					int currentval = p[i][j].getValue();
-//					if(lastval == currentval &&  lastval != 0){
-//						p[lastloc[0]][lastloc[1]].setValue(lastval+currentval);
-//						p[i][j].setValue(0);
-//						lastval = 0;
-//						lastloc[0] = -1;
-//						lastloc[1] = -1;
-//					}else if(currentval!=0){
-//						lastval = currentval;
-//						lastloc[0] = i;
-//						lastloc[1] = j;
-//					}
-//				}
-//			}
-//		}else if(dir == 'l'){
-//			for(int i = 0; i<height; i++){
-//				int lastval = 0;
-//				int[] lastloc = {-1, -1};
-//				for(int j = 0; j<width; j++){
-//					int currentval = p[i][j].getValue();
-//					if(lastval == currentval &&  lastval != 0){
-//						p[lastloc[0]][lastloc[1]].setValue(lastval+currentval);
-//						p[i][j].setValue(0);
-//						lastval = 0;
-//						lastloc[0] = -1;
-//						lastloc[1] = -1;
-//					}else if(currentval!=0){
-//						lastval = currentval;
-//						lastloc[0] = i;
-//						lastloc[1] = j;
-//					}
-//				}
-//			}
-//		}else if(dir == 'r'){
-//			for(int i = height-1; i>=0; i--){
-//				int lastval = 0;
-//				int[] lastloc = {-1, -1};
-//				for(int j = 0; j<width; j++){
-//					int currentval = p[i][j].getValue();
-//					if(lastval == currentval &&  lastval != 0){
-//						p[lastloc[0]][lastloc[1]].setValue(lastval+currentval);
-//						p[i][j].setValue(0);
-//						lastval = 0;
-//						lastloc[0] = -1;
-//						lastloc[1] = -1;
-//					}else if(currentval!=0){
-//						lastval = currentval;
-//						lastloc[0] = i;
-//						lastloc[1] = j;
-//					}
-//				}
-//			}
-//		}
+
 		
 	}
+	
 	public boolean shift(char dir){
-		Piece[][] p = board.getBoard();
 		boolean valid = false;
-		if(dir == 'u'){
-			boolean shifted = false;
-			while(!shifted){
-				shifted = true;
-				for(int j = 0; j<width; j++){
-					int lastval = 0;
-					int[] lastloc = {-1, -1};
-					for(int i = 0; i<height; i++){
-						int currentval = p[i][j].getValue();
-						if(currentval == 0){
-							lastloc[0] = i;
-							lastloc[1] = j;
-						}else if(currentval!=0){
-							if(lastloc[0]!=-1){
-								shifted = false;
-								p[lastloc[0]][lastloc[1]].setValue(p[i][j].getValue());
-								p[i][j].setValue(0);
-								lastloc[0] = -1;
-								lastloc[1] = -1;
-								valid = true;
-							}
-						}
-					}
-				}
-			}
-		}else if(dir == 'd'){
-			boolean shifted = false;
-			while(!shifted){
-				shifted = true;
-				for(int j = 0; j<width; j++){
-					int lastval = 0;
-					int[] lastloc = {-1, -1};
-					for(int i = height-1; i>=0; i--){
-						int currentval = p[i][j].getValue();
-						if(currentval == 0){
-							lastloc[0] = i;
-							lastloc[1] = j;
-						}else if(currentval!=0){
-							if(lastloc[0]!=-1){
-								shifted = false;
-								p[lastloc[0]][lastloc[1]].setValue(p[i][j].getValue());
-								p[i][j].setValue(0);
-								lastloc[0] = -1;
-								lastloc[1] = -1;
-								valid = true;
-							}
-						}
-					}
-				}
-			}
+		Piece[][] p = board.getBoard();
+		int iEnd = 0;
+		int iStart = 0;
+		int jEnd = 0;
+		int jStart = 0;
+		char orient = '\0';
+		if(dir == 'r'){
+			iStart = 0;
+			iEnd = width;
+			jStart = height-1;
+			jEnd = -1;
+			orient = 'h';
 		}else if(dir == 'l'){
-			boolean shifted = false;
-			while(!shifted){
-				shifted = true;
-				for(int i = 0; i<height; i++){
-					int lastval = 0;
-					int[] lastloc = {-1, -1};
-					for(int j = 0; j<width; j++){
-						int currentval = p[i][j].getValue();
-						if(currentval == 0){
-							lastloc[0] = i;
-							lastloc[1] = j;
-						}else if(currentval!=0){
-							if(lastloc[0]!=-1){
-								shifted = false;
-								p[lastloc[0]][lastloc[1]].setValue(p[i][j].getValue());
-								p[i][j].setValue(0);
-								lastloc[0] = -1;
-								lastloc[1] = -1;
-								valid = true;
-							}
-						}
+			iStart = 0;
+			iEnd = width;
+			jStart = 0;
+			jEnd = height;
+			orient = 'h';
+		}else if(dir == 'd'){
+			iStart = 0;
+			iEnd = width;
+			jStart = height-1;
+			jEnd = -1;
+			orient = 'v';
+		}else if(dir == 'u'){
+			iStart = 0;
+			iEnd = width;
+			jStart = 0;
+			jEnd = height;
+			orient = 'v';
+		}
+		boolean shifted = false;
+		while(!shifted){
+			shifted = true;
+			for(int i = iStart; i!=iEnd; i += (int)(Math.signum(iEnd-iStart))){
+				int lastval = 0;
+				int[] lastloc = {-1, -1};
+				
+				for(int j = jStart; j!=jEnd; j += (int)(Math.signum(jEnd-jStart))){
+					int currentval = 0;
+					int x = -1;
+					int y = -1;
+					if(orient == 'h'){
+						x=i;
+						y=j;
+					}else if(orient == 'v'){
+						x=j;
+						y=i;
 					}
-				}
-			}
-		}else if(dir == 'r'){
-			boolean shifted = false;
-			while(!shifted){
-				shifted = true;
-				for(int i = 0; i<height; i++){
-					int lastval = 0;
-					int[] lastloc = {-1, -1};
-					for(int j = width-1; j>=0; j--){
-						int currentval = p[i][j].getValue();
-						if(currentval == 0){
-							lastloc[0] = i;
-							lastloc[1] = j;
-						}else if(currentval!=0){
-							if(lastloc[0]!=-1){
-								shifted = false;
-								p[lastloc[0]][lastloc[1]].setValue(p[i][j].getValue());
-								p[i][j].setValue(0);
-								lastloc[0] = -1;
-								lastloc[1] = -1;
-								valid = true;
-							}
+					currentval = p[x][y].getValue();
+					if(currentval == 0){
+						lastloc[0] = x;
+						lastloc[1] = y;
+					}else if(currentval!=0){
+						if(lastloc[0]!=-1){
+							shifted = false;
+							p[lastloc[0]][lastloc[1]].setValue(p[x][y].getValue());
+							p[x][y].setValue(0);
+							lastloc[0] = -1;
+							lastloc[1] = -1;
+							valid = true;
 						}
 					}
 				}
 			}
 		}
-		return valid;
 		
+		return valid;
+
 	}
+	
+	
 	public boolean move(char dir){
-//		combine(dir);
-//		shift(dir);
 		boolean cvalid = combine(dir);
 		boolean svalid = shift(dir);
 		return cvalid||svalid;
 	}
 }
-
-
-
