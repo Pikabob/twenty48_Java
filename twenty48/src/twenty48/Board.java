c02251380d8958f2152b00bf8243de9fa2b95aaf
Pikabob <pikabob25@gmail.com>
2014-05-07 11:35:25 -0700

    Cleanup

--- twenty48/src/twenty48/Board.java
+++ twenty48/src/twenty48/Board.java
@@ -4,27 +4,11 @@ public class Board {
 	Piece[][] board;
 	public Board(int width, int height){
 		board = new Piece[width][height];
-//		for(Piece[] i : board){
-//			for(Piece j : i){
-//				j = new Piece(0);
-//			}
-//		}
 		for(int i = 0; i < board.length; i++){
 			for(int j = 0; j < board[i].length; j++){
 				board[i][j] = new Piece(0);
 			}
 		}
-//		board[0][0] = new Piece(2);
-//		board[0][1] = new Piece(4);
-//		board[0][2] = new Piece(8);
-//		board[0][3] = new Piece(16);
-//		board[1][0] = new Piece(32);
-//		board[1][1] = new Piece(64);
-//		board[1][2] = new Piece(128);
-//		board[1][3] = new Piece(256);
-//		board[2][0] = new Piece(512);
-//		board[2][1] = new Piece(1024);
-//		board[2][2] = new Piece(2048);
 	}
 	public void print(){
 		for(Piece[] i : board){
@@ -32,7 +16,6 @@ public class Board {
 				for(Piece j : i){
 					String[] s = j.getRepresentation();
 					System.out.print(s[row] + "  ");
-					
 				}
 				System.out.println();
 			}
--- twenty48/src/twenty48/Main.java
+++ twenty48/src/twenty48/Main.java
@@ -4,10 +4,5 @@ public class Main {
 	public static void main(String[] args) {
 		Game g = new Game();
 		g.start();
-//		Piece p = new Piece();
-//		String[] s = p.getRepresentation();
-//		for(String s1: s){
-//			System.out.println(s1);
-//		}
 	}
 }
